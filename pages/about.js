197040b0b0800d1dc6488faa7c81a2324230d96e
IngEoGeoBuk <>
2021-02-18 00:03:09 +0900

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head';
+
+const about = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About </title>
+                <meta name="keywords" content="ninjas"></meta>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            </div>
+        </>
+    )
+}
+
+export default about
